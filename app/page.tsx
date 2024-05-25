08f5e78923e8fcf65c634f9d19791ac1928c08bc
Akash Shukla <49320497+theakashshukla@users.noreply.github.com>
2024-05-26 03:16:15 +0530

    feat: Add GitHub icon to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { Icons } from "@/components/icons";
 import Quest from "./quest/page";
 
 export default function Home() {
@@ -5,6 +6,7 @@ export default function Home() {
     <main className="flex justify-center m-2">
       
       <Quest />
+      <Icons.gitHub className="w-6 h-6" />
 
     </main>
   );
